609b2e6ab47f3b1be6f57432f9349d24d96d4873
Tahir Emre AK <tahiremreak@gmail.com>
2025-01-27 19:20:09 +0300

    First steps
    - Package-lock.json oluşturuldu.
    - Components klasörü oluşturuldu.
    - App.jsx return kısmı temizlendi.

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import workintech from '/workintech.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
